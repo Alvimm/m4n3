a7c87c28dcfa3e8b6c218cecb1a3549867802a04
alvimm <filipe2012alvim@gmail.com>
2024-04-30 15:20:30 -0300

    Refactor MainActivity.kt and add dialog_layout.xml

--- app/src/main/java/com/doma/domaapp/MainActivity.kt
+++ app/src/main/java/com/doma/domaapp/MainActivity.kt
@@ -2,8 +2,12 @@ package com.doma.domaapp
 
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
+import android.view.LayoutInflater
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ListView
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import java.util.Locale
 
@@ -21,6 +25,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+
         val messages = mapOf(
             "Filipe" to "Reunião às 10 horas amanhã",
             "João" to "Entrega do projeto na sexta-feira",
@@ -31,11 +36,30 @@ class MainActivity : AppCompatActivity() {
         val listView = findViewById<ListView>(R.id.listView)
         val adapter = ArrayAdapter(this, R.layout.list_item, messages.keys.toList())
         listView.adapter = adapter
-
         listView.setOnItemClickListener { _, _, position, _ ->
             val name = messages.keys.elementAt(position)
             val message = messages[name]
-            textToSpeech.speak("Ouvindo a mensagem de $name: $message", TextToSpeech.QUEUE_FLUSH, null, "")
+
+            val dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_layout, null)
+            textToSpeech.speak("Gostaria de ouvir a mensagem de $name?", TextToSpeech.QUEUE_FLUSH, null, "")
+            val dialogMessage = dialogView.findViewById<TextView>(R.id.dialog_message)
+            val positiveButton = dialogView.findViewById<Button>(R.id.dialog_positive_button)
+            val negativeButton = dialogView.findViewById<Button>(R.id.dialog_negative_button)
+
+            dialogMessage.text = "Gostaria de ouvir a mensagem de $name?"
+
+            val alertDialog = AlertDialog.Builder(this).apply {
+                setView(dialogView)
+                create()
+            }.show()
+
+            positiveButton.setOnClickListener {
+                textToSpeech.speak("Ouvindo a mensagem de $name: $message", TextToSpeech.QUEUE_FLUSH, null, "")
+                alertDialog.dismiss()
+            }
+            negativeButton.setOnClickListener {
+                alertDialog.dismiss()
+            }
         }
     }
 
